bc74ee48337792fe90b0b90b7afba5842ce7bed9
Abhinav Yata <15388677+ay1610@users.noreply.github.com>
2025-09-15 14:40:29 -0700

    fix(layout): update TrendingTypeLayout to handle params as a promise and improve heading logic fix(card): refine handleMouseMove type for better event handling fix(prisma): remove unused withAccelerate extension for PrismaClient

--- components/ui/3d-card.tsx
+++ components/ui/3d-card.tsx
@@ -19,13 +19,12 @@ export const CardContainer = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const [isMouseEntered, setIsMouseEntered] = useState(false);
 
-  const handleMouseMove = (e: MouseEvent) => {
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (!containerRef.current) return;
     const { left, top, width, height } = containerRef.current.getBoundingClientRect();
     const x = (e.clientX - left - width / 2) / 25;
     const y = (e.clientY - top - height / 2) / 25;
     containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
-
   };
 
   const handleMouseEnter = () => {

d78039611fa0506de49eebc529f50eb475a7f425
Abhinav Yata <15388677+ay1610@users.noreply.github.com>
2025-09-18 14:49:00 -0700

    fix: replace TrendingMoviesClient with TrendingMoviesPage in home page to improve load times

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,13 @@
 import { getProtectedUser } from "@/lib/auth-helpers";
-import { TrendingMoviesClient } from "@/components/trending-movies/TrendingMoviesClient";
 import { Banner } from "@/components/ui/banner";
+import TrendingMoviesPage from "../trending/movies/page";
 
 export default async function Page() {
   const user = await getProtectedUser();
   return (
     <>
       <Banner>{`Welcome, ${user?.name}! Email: ${user?.email}`}</Banner>
-      <TrendingMoviesClient />
+      <TrendingMoviesPage />
     </>
   );
 }
